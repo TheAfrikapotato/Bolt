20cc992ec68c9b18301493fa314f811c5ac05f9d
EnchantedDonutStudioz <EnchantedDonutStudioz@gmail.com>
2024-11-23 21:26:49 -0500

    oi oi oi baaka

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 import { join } from "node:path";
 import { hostname } from "node:os";
 import wisp from "wisp-server-node";
-import rammerhead from '@rubynetwork/rammerhead';
-import { createRammerhead, shouldRouteRh, routeRhUpgrade, routeRhRequest } from '@rubynetwork/rammerhead';
+
 
 
 const __dirname = process.cwd();
@@ -21,12 +20,7 @@ app.use("/epoxy/", express.static(epoxyPath));
 app.use("/baremux/", express.static(baremuxPath));
 
 const server = createServer();
-const rh = createRammerhead({
-    logLevel: 'debug', //Options are: disabled, debug, traffic, info, warn, error (default: debug)
-    reverseProxy: false, //whether or not this server is running behind a reverse proxy (default: false)
-    disableLocalStorageSync: false, //disable localstorage sync (not recommended) (default: false)
-    disableHttp2: false //disable http2 usage (default: false) (NOT RECOMMENDED)
-});
+
 
 app.use((req, res) => {
     res.status(404);
@@ -36,20 +30,11 @@ app.use((req, res) => {
 server.on("request", (req, res) => {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
     res.setHeader("Cross-Origin-Embedder-Policy", "anonymous");
-    if (shouldRouteRh(req)) {
-        routeRhRequest(rh /* from the createRammerhead function MUST be passed */, req, res)
-    }
-    else {
-        app(req, res);
-    }
 });
 
 server.on("upgrade", (req, socket, head) => {
     if (req.url.endsWith("/wisp/")) {
         wisp.routeRequest(req, socket, head)
-    }
-    else if (rammerhead.shouldRouteRh(req)) {
-        rammerhead.routeRhUpgrade(rh, req, socket, head)
     }
     else {
         socket.end()
